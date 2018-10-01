93cf54232608496b54d362196cf572e781a7a28f
sarahpai <sarahpai@github.com>
2018-10-01 07:27:09 -0400

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -19,7 +19,7 @@ end
 def word_substituter(tweet)
   array = tweet.split(" ") #puts all the tweet indivdual array to allow iterate
     #binding.pry
-     array.collect do |words|   #itereate through tweet block method
+     array.collect do |words|   #itereate through tweet blocks
         if dictionary.keys.include?(words.downcase)  #dictionay.key => pulls only keys in dictionary then check to see if keys are included in tweet
           words = dictionary[words.downcase] #downcased so it will match with key
         else
@@ -45,8 +45,16 @@ end
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140
-      return "#{tweet[0..136}..."
+      return "#{tweet[0..136]}..."  #interpolate tweet to truncate the tweet => "text here is displayed up till set number..."
   else
   tweet
   end
 end
+
+#If you want "Hello, World!" to be limited to the first five letters, you can do:
+
+# str = "Hello, World!"
+# str[0...5] # => "Hello"
+# If you want an ellipses, just interpolate it:
+
+# "#{str[0...5]}..." #=> "Hello..."
